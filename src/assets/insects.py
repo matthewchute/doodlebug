07fdd3df56960abd6e893aaa145a928264e770e5
matthewchute <mchute@sfu.ca>
2021-10-05 11:22:09 -0700

    add moving attributes

--- src/assets/insects.py
+++ src/assets/insects.py
@@ -5,6 +5,15 @@ class insect:
     
     def getType(self):
         return self.type
+    
+    def didMove(self):
+        return self.moved 
+
+    def move(self):
+        self.moved = True
+
+    def rest(self):
+        self.moved = False
 
 class ant(insect):
     def __init__(self):
